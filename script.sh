15903d69b504530878dadd06292e2765c053547f
Rehan Raza <70847285+Rehan-Raza@users.noreply.github.com>
2023-01-26 13:37:31 +0530

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+inotifywait -m /home/rehan1114/Desktop -e create -e moved_to -e delete | 
+while read path action file; do
+echo $file $action $path ;
+        if [[ "$file" =~ .*txt$ || "$file" =~ .*log$ ]]; then 
+            mv $file $file_`date +"%m-%d-%y"`_$file”;
+        fi
+    done
